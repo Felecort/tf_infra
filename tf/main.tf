0c1e925c297a5f387e001a0ae5932fa084f6dc0d
Felecort <dotpointen@gmail.com>
2024-12-17 21:32:15 +0300

    upd config

--- tf/main.tf
+++ tf/main.tf
@@ -10,3 +10,15 @@ resource "openstack_compute_instance_v2" "my_instance" {
 
   security_groups = [var.security_group]
 }
+
+# Создание тома размером 20 ГБ
+resource "openstack_blockstorage_volume_v3" "my_volume" {
+  name        = "${var.instance_name}_volume"
+  size        = 20 # размер в ГБ
+}
+
+# Присоединение тома к виртуальной машине
+resource "openstack_compute_volume_attach_v2" "my_volume_attach" {
+  instance_id = openstack_compute_instance_v2.my_instance.id
+  volume_id   = openstack_blockstorage_volume_v3.my_volume.id
+}
