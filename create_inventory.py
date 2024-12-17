0af6015ca844cf0d982da0c6f2d430d656ba7da1
Felecort <dotpointen@gmail.com>
2024-12-17 22:11:08 +0300

    change script

--- create_inventory.py
+++ create_inventory.py
@@ -1,7 +1,9 @@
 import os
+import subprocess
 
 if __name__ == "__main__":
-    ip = os.system("cd ~/tf_infra/tf/ && terraform output -raw instance_ip")
-    s = f'[servers]\nbatashev_infra ansible_host={ip} ansible_user=ubuntu ansible_ssh_private_key_file=~/.ssh/openstack'
-    with open('~//home/ubuntu/tf_infra/ans/inventory', 'w') as f:
+    # ip = os.system("cd ~/tf_infra/tf/ && terraform output -raw instance_ip")
+    ip = subprocess.check_output("cd /home/ubuntu/tf_infra/tf/ && terraform output -raw instance_ip", shell=True)
+    s = f'[servers]\nbatashev_infra ansible_host={str(ip.decode("utf-8"))} ansible_user=ubuntu ansible_ssh_private_key_file=~/.ssh/openstack'
+    with open('/home/ubuntu/tf_infra/ans/inventory', 'w') as f:
         f.write(s)
