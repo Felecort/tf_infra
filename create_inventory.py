f807f9f64f0b9c6c5ef581805c9350f914307b62
Felecort <dotpointen@gmail.com>
2024-12-18 20:26:17 +0300

    save ip in file

--- create_inventory.py
+++ create_inventory.py
@@ -2,6 +2,9 @@ import subprocess
 
 if __name__ == "__main__":
     ip = subprocess.check_output("cd tf/ && terraform output -raw instance_ip", shell=True)
-    s = f'[servers]\nbatashev_infra ansible_host={str(ip.decode("utf-8"))} ansible_user=ubuntu ansible_ssh_private_key_file=~/.ssh/openstack'
+    ip = str(ip.decode("utf-8"))
+    s = f'[servers]\nbatashev_infra ansible_host={ip} ansible_user=ubuntu ansible_ssh_private_key_file=~/.ssh/openstack'
     with open('ans/inventory', 'w') as f:
         f.write(s)
+    with open('~/.instance_ip', 'w') as f:
+        f.write(ip)
